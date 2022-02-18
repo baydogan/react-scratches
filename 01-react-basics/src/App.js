14bf50fbb8cdfca6e1f4ab9b356805da769c9f06
baydogan <aydoganbugra93@gmail.com>
2022-02-18 15:43:48 +0300

    conditional templates

--- 01-react-basics/src/App.js
+++ 01-react-basics/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [showTexts, setShowTexts] = useState(true);
+  const [name, setName] = useState("change");
+  const [list, setList] = useState([
+    { title: "First Text", id: 1 },
+    { title: "Second Text", id: 2 },
+    { title: "Third Text", id: 3 },
+  ]);
+
+  const handleClick = () => {
+    setName("changed");
+  };
+
+  const clickButton = (id) => {
+    setList(
+      list.filter((list) => {
+        return id !== list.id;
+      })
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!showTexts && (
+        <div>
+          <button onClick={() => setShowTexts(true)}>Show texts</button>
+        </div>
+      )}
+
+      {showTexts && (
+        <div>
+          <button onClick={() => setShowTexts(false)}>Hide texts</button>
+        </div>
+      )}
+      <h1>This will be {name}</h1>
+      <button onClick={handleClick}>Click</button>
+      {showTexts &&
+        list.map((list, index) => (
+          <div key={list.id}>
+            <h2>
+              {index} - {list.title}
+            </h2>
+            <button onClick={() => clickButton(list.id)}>Delete text</button>
+          </div>
+        ))}
     </div>
   );
 }
